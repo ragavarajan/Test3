4389ec197ba9e48abfbb31fc1843a9734d3fc0b9
innoppl <innoppl@inno0252-PC>
2020-02-12 07:58:29 +0530

    push 3

--- Githubtest/src/Githubtestclass1.java
+++ Githubtest/src/Githubtestclass1.java
@@ -1,6 +1,17 @@
 
 public class Githubtestclass1 {
+	
+	
 
+	/*driver.get("https://mail.google.com");
+	Thread.sleep(3000);
+	driver.findElement(By.id("identifierId")).sendKeys("ragavarajan");
+	driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+	Thread.sleep(3000);
+	driver.findElement(By.xpath("//input[@type='password']")).sendKeys("admin123");
+	driver.findElement(By.xpath("//span[@class='CwaK9']//*[text()='Next']")).click();*/
+	
+	
 	
 	
 	
